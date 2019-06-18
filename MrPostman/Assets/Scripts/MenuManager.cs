521401f5abe9e74c082902badc2802407990985e
Job_Elfrink <job.elfrink@gmail.com>
2019-06-19 01:07:35 +0200

    FULL GAME (Not able to click 2nd level)

---
+++ MrPostman/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void OpenScene(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void LevelRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void StopGame()
+    {
+        Application.Quit();
+    }
+}
